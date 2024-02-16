8c3b1475a46d2016015fddf65b7a0646263a36a1
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-17 00:52:12 +0600

    add /login, signup and logout

---
+++ server/src/api/authentication/controllers/doLogout.js
@@ -0,0 +1,13 @@
+const doLogout = (req, res) => {
+    res
+      .clearCookie("token", { maxAge: 0 })
+      .clearCookie("token", {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      })
+      .send({ success: true, message: "logout successful"});
+  };
+  
+  module.exports = doLogout;
+  
