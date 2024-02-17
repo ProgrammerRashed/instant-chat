682c5116d5202884249381a1b78b47ae981c5875
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-17 15:02:26 +0600

    add /active user route

--- server/src/routes/authentication/index.js
+++ server/src/routes/authentication/index.js
@@ -1,6 +1,7 @@
 const doLogin = require("../../api/authentication/controllers/doLogin")
 const doLogout = require("../../api/authentication/controllers/doLogout")
 const doSignup = require("../../api/authentication/controllers/doSignup")
+const getUser = require("../../api/authentication/controllers/getUser")
 
 const router = require("express").Router()
 
@@ -8,5 +9,6 @@ const router = require("express").Router()
 router.post("/signup", doSignup)
 router.post("/login", doLogin)
 router.post("/logout", doLogout)
+router.post("/user", getUser)
 
 module.exports = router
