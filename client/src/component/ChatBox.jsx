1bf578ad0f5b36e5f4a27e662fdadfc5cb30e173
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-17 11:32:47 -0500

    improve /chat box design

--- client/src/component/ChatBox.jsx
+++ client/src/component/ChatBox.jsx
@@ -5,17 +5,21 @@ import { IoMdInformationCircleOutline } from "react-icons/io";
 import { FaRegImage } from "react-icons/fa6";
 import "react-chat-elements/dist/main.css";
 import { MessageBox } from "react-chat-elements";
+import Image from "next/image";
 
 const ChatBox = () => {
   return (
-    <div className="z-[999]">
+    <div className="z-[999] w-full">
       <div className="bg-[url('https://i.ibb.co/NyZkx2Q/e86c13b0-4e16-4c56-b5b5-1a90acbea77c-naruwhatsappwallpaperdark.webp')] overflow-y-auto  relative h-screen w-full">
+      {/* USER NAME HEADER */}
         <div className="bg-[#121C22] pl- border-b z-[999] border-slate-700 text-[#a0bcd3] shadow- p-2 flex gap-x-3 justify-between items-center sticky top-0 ">
           <div className="flex items-center gap-3">
-            <img
+            <Image
+            width={50}
+            height={50}
               className="w-[50px] h-[50px] rounded-full"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Ne7oVV6Lx9uAnmJDUZrrLcGy8yzo1sXdpQ&usqp=CAU"
-              alt=""
+              alt="user-image"
             />
             <h1 className="text-xl text-white font-semibold">Polash Ahmed</h1>
           </div>
@@ -33,6 +37,8 @@ const ChatBox = () => {
           </div>
         </div>
 
+
+{/* MESSAGES */}
         <div className="overflow-y-auto px-5 py-3 text-slate-700">
           <MessageBox
             position={"left"}
@@ -126,17 +132,18 @@ const ChatBox = () => {
           />
         </div>
 
-        <form className="fixed  bottom-0 p-3 w-full bg-[#121C22] shadow-lg flex flex-row gap-2 items-center">
-          <div className="flex w-full items-center gap-2">
-            <div>
+{/* MESSAGE INPUT BUTTON */}
+        <form className="fixed w-[75%] bottom-0 p-3 bg-[#121C22] flex justify-center items-center gap-5 overflow-hidden">
+        
+            <div className="w-[5%] relative flex justify-center items-center">
               <label
-                className="h-[42px] w-[42px] bg-[#0B1114]  mx-auto rounded-sm"
+                className="rounded-sm"
                 htmlFor="attachment"
               >
-                <FaRegImage className="mt-2 text-white text-xl " />
+                <FaRegImage className="mt-2 h-full w-full text-white text-xl " />
               </label>
               <input
-                className="hidden"
+                className="absolute hidden"
                 type="file"
                 name="attachment"
                 id="attachment"
@@ -145,24 +152,24 @@ const ChatBox = () => {
 
             {/* content */}
 
-            <div className="">
+            <div className="w-[80%]">
               <input
                 type="text"
                 name="textField"
                 placeholder="write message"
-                className="text-sm rounded-lg outline-none block w-[67vw] py-2.5 px-3 bg-[#0B1114] placeholder-gray-400 text-white border border-[#0B1114] focus:outline-0 focus:border-[#3B82F6]"
+                className="text-sm w-full rounded-lg outline-none block py-2.5 px-3 bg-[#0B1114] placeholder-gray-400 text-white border border-[#0B1114] focus:outline-0 focus:border-[#3B82F6]"
               />
             </div>
 
-            <div>
+            <div className="w-[10%]">
               <button
-                className="text-white bg-[#0B1114] px-4 py-2.5"
+                className="text-white bg-[#0B1114] rounded w-full px-4 py-2.5"
                 type="submit"
               >
                 Send
               </button>
             </div>
-          </div>
+
         </form>
       </div>
     </div>
