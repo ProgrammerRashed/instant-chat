b8ea0f7682fbfded71b5ecebea3f74fa7aee9c61
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-18 18:16:02 +0600

    fix /token not setting issue

---
+++ client/src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+
+export function middleware(request) {
+  const pathName = request.nextUrl.pathname;
+
+  const isPublicPath = pathName === "/login" || pathName === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  
+  //Handle Public Request
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  //handle private path request
+  if (!isPublicPath && !token) {
+    if (pathName === "/") {
+      return null;
+    } else {
+      return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/login", "/signup"],
+};
