8462039702b4b62e2aad70a51b8f32ffc2264df9
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-19 22:31:59 +0600

    fix /get issue on server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,11 @@
 const applyMiddleware = require("./middlewares");
 const globalErrorHandler = require("./utils/globalErrorHandler");
 require("dotenv").config();
-
+const express = require("express")
 const authRoutes = require('./routes/authentication');
 const messageRoutes = require("./routes/message")
 const userRoutes = require("./routes/users");
-const { app } = require("./socket/socket");
+const app = express();
 applyMiddleware(app);
 
 
